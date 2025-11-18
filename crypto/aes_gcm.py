4ad08442111d6a29a8a2f3fe7caac5c9be51b463
Mohammed Busaleh <moh.s.busaleh@gmail.com>
2025-11-18 13:39:35 +0300

    aes_gcm and all its function is DONE!

--- crypto/aes_gcm.py
+++ crypto/aes_gcm.py
@@ -1,1 +1,18 @@
 #Step 2 -- By Mohammed
+from cryptography import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
+
+def generate_key():
+    return AESGCM.generate_key(bit_length=256)
+
+def encrypt_aes_gcm(plaintext: bytes, key: bytes):
+    iv = os.urandom(12)
+    aesgcm = AESGCM(key)
+    ciphertext = aesgcm.encrypt(iv, plaintext, None)
+    return ciphertext, iv
+
+def decrypt_aes_gcm(iv: bytes, ciphertext: bytes, tag: bytes, key: bytes):
+    aesgcm = AESGCM(key)
+    decryptedText = aesgcm.decrypt(iv, ciphertext + tag, None)
+    return decryptedText
