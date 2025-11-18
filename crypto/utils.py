d420e073ffaf48e1b9bf1c4301cd6e236db09b57
Mohammed Busaleh <moh.s.busaleh@gmail.com>
2025-11-18 13:51:59 +0300

    Created Base64 decryption that would help with the display

--- crypto/utils.py
+++ crypto/utils.py
@@ -1,3 +1,11 @@
+import base64
 
-def encrypt_message(message):
-    return 
+def display_message(encMsg):
+    output = base64.b64encode(encMsg)
+    output_str = output.decode('utf-8')
+    return output_str
+  
+def parse_message(encMsg):
+    output = base64.b64encode(encMsg)
+    output_str = output.decode('utf-8')
+    return output_str
