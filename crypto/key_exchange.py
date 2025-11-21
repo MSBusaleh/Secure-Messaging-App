f8ddfc4456fa75eb98e052cf6f9b198f56aaa8c7
Mohammed Busaleh <moh.s.busaleh@gmail.com>
2025-11-21 23:36:52 +0300

    Step 3 (RSA enc) is DONE!

--- crypto/key_exchange.py
+++ crypto/key_exchange.py
@@ -1,1 +1,34 @@
 # Step 3 -- By Mohammed
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+
+
+def generate_rsa_keypair():
+    private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_key_with_rsa(aes_key: bytes, public_key):
+    encrypted_key = public_key.encrypt(
+                                    aes_key,
+                                    padding.OAEP(
+                                                  mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                  algorithm=hashes.SHA256(),
+                                                  label=None
+                                                )
+                                    )
+    return encrypted_key
+
+def decrypt_key_with_rsa(encrypted_key: bytes, private_key):
+    decrypted_key = private_key.decrypt(
+                                        encrypted_key,
+                                        padding.OAEP(
+                                                      mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                      algorithm=hashes.SHA256(),
+                                                      label=None
+                                                    )
+                                      )
+    return decrypted_key
